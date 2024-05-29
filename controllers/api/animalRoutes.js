7493c00bc6fdc62ccd5fb3f8332eb6853d048639
ninadri <159666628+ninadri@users.noreply.github.com>
2024-05-28 19:38:01 -0500

    added a PUT route for updating inventory

--- controllers/api/animalRoutes.js
+++ controllers/api/animalRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Animal } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+// CREATES an animal
 router.post("/", withAuth, async (req, res) => {
   try {
     const newAnimal = await Animal.create({
@@ -15,6 +16,33 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
+//Updates an animal
+router.put("/:id/inventory", async (req, res) => {
+  try {
+    const { inventory } = req.body;
+    if (inventory === undefined) {
+      return res.status(400).json({ message: "Inventory value is required" });
+    }
+    // Update the animal's inventory
+    const animalData = await Animal.update(
+      { inventory },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!animalData[0]) {
+      res.status(404).json({ message: "No animal with this id!" });
+      return;
+    }
+    res.status(200).json(animalData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE an animal
 router.delete("/:id", withAuth, async (req, res) => {
   try {
     const animalData = await Animal.destroy({
