423e44869f13c4de82e01355ddf59c5f95446e18
wkromrei <wkromrei@gmail.com>
2024-06-04 10:13:30 -0500

    modified env file, as well as connection.js to make deployment on render.com functional.

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.DB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "localhost",
+      host: process.env.DB_HOSTNAME,
       dialect: "postgres",
     }
   );
