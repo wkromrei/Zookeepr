1dd8fd86c3ab8b36f50f9d7e995598a3260b58f2
Aksana <aksa.hleb@gmail.com>
2024-06-06 18:22:12 -0500

    Changed sever.js

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ const sess = {
   secret: "Super secret secret",
   cookie: {
     maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
